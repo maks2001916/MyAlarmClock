963f7e6bc97a72259f438f292ce010db3a6b06f3
maks2001916 <mamonov.maksim.20003@gmail.com>
2025-01-22 18:02:25 +0500

    доработка функциональности

--- app/src/main/java/com/example/myalarmclock/AlarmWentOff.kt
+++ app/src/main/java/com/example/myalarmclock/AlarmWentOff.kt
@@ -10,6 +10,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.system.exitProcess
 
 class AlarmWentOff : AppCompatActivity() {
 
@@ -24,6 +25,12 @@ class AlarmWentOff : AppCompatActivity() {
 
         toolbarTB = findViewById(R.id.toolbarWentOff)
         imageGifIV = findViewById(R.id.turnOffGIV)
+        turnOffBTN = findViewById(R.id.turnOffBTN)
+
+        turnOffBTN.setOnClickListener {
+            finish()
+            exitProcess(0)
+        }
 
     }
 
